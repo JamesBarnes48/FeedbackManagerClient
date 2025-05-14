641f4db58fbc035ddcb89107632fee02ad4b262e
james.barnes <james.barnes@solirius.com>
2025-05-14 11:56:22 +0100

    implement clientside routing

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,9 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import FeedbackInput from './components/FeedbackInput.vue'
-import AllFeedbackContainer from './components/FeedbackContainer.vue';
-import type { Feedback } from './interfaces/Feedback';
-
-const feedbacks = ref<Feedback[]>([]);
-
-function updateFeedbacks(data: Feedback){
-  feedbacks.value.push(data);
-  console.info(feedbacks.value);
-}
-
+  import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div>
-    <h1 class="page-title">Feedback Manager</h1>
-    <h3 class="subtitle">Enter New Feedback:</h3>
-    <FeedbackInput @submitFeedback="updateFeedbacks" />
-    <h3 class="subtitle">Your Feedback:</h3>
-      <AllFeedbackContainer :feedbackArray="feedbacks" />
-  </div>
+  <RouterView />
 </template>
 
 <style scoped>
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+
+//need this so typescript can import vue files!!
+declare module '*.vue' {
+    import { DefineComponent } from 'vue'
+    const component: DefineComponent<{}, {}, any>
+    export default component
+  }
