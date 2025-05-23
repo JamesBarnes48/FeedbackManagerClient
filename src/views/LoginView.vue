90e9539661aaeab78169479306ccfada5fee6132
james.barnes <james.barnes@solirius.com>
2025-05-23 13:40:17 +0100

    add tooltip to login fields

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -15,8 +15,8 @@
     }
 
     function login(){
-        if(!/^[a-zA-Z0-9_]{3,30}$/.test(usernameInput.value)) return triggerError('Invalid username field. Username must only consist of letters, digits and underscores and be at least 3 characters long');
-        if(!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{6,32}$/.test(passwordInput.value)) return triggerError('Invalid password field. Password must contain at least one uppercase letter, lowercase letter and digit and be at least 6 characters long');
+        if(!/^[a-zA-Z0-9_]{3,30}$/.test(usernameInput.value)) return triggerError('Invalid username field.');
+        if(!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{6,32}$/.test(passwordInput.value)) return triggerError('Invalid password field.');
 
     }
 </script>
@@ -28,11 +28,19 @@
         <div class="form-container">
             <div class="input-row">
                 <label>Username</label>
-                <input v-model="usernameInput" type="text">
+                <v-tooltip location="top" text="Username must only consist of letters, digits and underscores and be at least 3 characters long">
+                    <template v-slot:activator="{ props }">
+                        <input v-bind="props" v-model="usernameInput" type="text">
+                    </template>
+                </v-tooltip>
             </div>
             <div class="input-row">
                 <label>Password</label>
-                <input v-model="passwordInput" type="text">
+                <v-tooltip location="top" text="Password must contain at least one uppercase letter, lowercase letter and digit and be at least 6 characters long">
+                    <template v-slot:activator="{ props }">
+                        <input v-bind="props" v-model="passwordInput" type="text">
+                    </template>
+                </v-tooltip>
             </div>
         </div>
         <span class="button submit-button" @click="login">Login</span>
