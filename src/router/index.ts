f4a247b58f2dd932af3ade396bf15ae353559dbb
james.barnes <james.barnes@solirius.com>
2025-05-23 11:51:22 +0100

    create simple auth-based routing for new login view

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/login',
+      name: 'login',
+      component: LoginView
+    },
+    {
       path: '/',
       name: 'home',
       component: HomeView,
+      meta: {requiresAuth: true}
     }
   ],
+})
+
+router.beforeEach((to, from, next) => {
+  const auth = true; //say we are authed for now
+  if(to.meta.requiresAuth && !auth) next({name: 'login'})
+  else next();
 })
 
 export default router
---
+++ src/views/LoginView.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+    import { ref } from 'vue';
+    import ErrorBanner from '../components/ErrorBanner.vue';
+    import api from '../api';
+
+    const errorMessage = ref('');
+    const showError = ref(false);
+
+    function triggerError(message: string){
+        errorMessage.value = message;
+        showError.value = true;
+    }
+</script>
+
+<template>
+    <div>
+        <ErrorBanner v-model:show="showError" :message="errorMessage" />
+        <h1 class="page-title">Welcome to Feedback Manager</h1>
+        <a href="/">login</a>
+    </div>
+</template>
+
+<style scoped>
+</style>
