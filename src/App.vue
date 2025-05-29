8f8df11d4cc9df740b3d944fe5d76c3e9a90e2bd
james.barnes <james.barnes@solirius.com>
2025-05-29 12:01:57 +0100

    make errorbanner able to handle other types of events

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,25 @@
 <script setup lang="ts">
   import { RouterView } from 'vue-router'
   import { ref } from 'vue';
-  import ErrorBanner from './components/ErrorBanner.vue';
+  import InfoBanner from './components/InfoBanner.vue';
 
-  const errorMessage = ref('');
-  const showError = ref(false);
+  const [bannerMessage, bannerType, showBanner] = [ref(''), ref('error'), ref(false)];
 
-  function triggerError(message: string){
-    errorMessage.value = message;
-    showError.value = true;
+  //elected to use this method so that we can maintain clear event handler names all the way until here
+  const triggerError = (message: string) => triggerBanner(message, 'error');
+  const triggerSuccess = (message: string) => triggerBanner(message, 'success');
+
+  function triggerBanner(message: string, type: string){
+    bannerMessage.value = message;
+    bannerType.value = type;
+    showBanner.value = true;
   }
+
 </script>
 
 <template>
-  <ErrorBanner v-model:show="showError" :message="errorMessage" />
-  <RouterView @error="triggerError" />
+  <ErrorBanner v-model:show="showBanner" :message="bannerMessage" :type="bannerType" />
+  <RouterView @error="triggerError" @success="triggerSuccess" />
 </template>
 
 <style scoped>
--- src/components/InfoBanner.vue
+++ src/components/InfoBanner.vue
@@ -2,15 +2,20 @@
   import { ref, watch } from 'vue'
 
   /* this makes use of Vue 3's v-model:propName syntax to have two-way control over the 'show' prop
-  we defined this component with the option v-model:show="showError" in HomeView.vue
-  this means the showError variable in the parent corresponds to the 'show' prop in this component.
+  we defined this component with the option v-model:show="showBanner" in HomeView.vue
+  this means the showBanner variable in the parent corresponds to the 'show' prop in this component.
   Its basically v-model except you get to name the prop! so you can have multiple v-models corresponding to different props
+
+  This component is designed to be multi-functional in the sense that it is a banner than can show errors, successes and general info based on its props
+  while there are loads of event handlers throughout the app (@error, @success) that fire up the stack, App.vue handles them and transposes them into props to show the correct banner
+  Single point of control over this component also reduces risk of interference, two banners at once etc.
   */
   
   export default {
-    name: 'ErrorBanner',
+    name: 'InfoBanner',
     props: {
       message: {type: String, required: true},
+      type: {type: String, required: true},
       show: {type: Boolean, required: true}
     },
     setup(props, { emit }) {
@@ -36,7 +41,7 @@
 
 <template>
     <transition name="slide-fade">
-      <div v-if="visible" class="error-banner">
+      <div v-if="visible" class="info-banner" :class="type">
         {{ message }}
       </div>
     </transition>
@@ -44,16 +49,24 @@
   
   
   <style scoped>
-  .error-banner {
+  .info-banner {
     position: fixed;
     top: 20px;
     left: 50%;
-    background-color: #f44336;
-    color: white;
     padding: 12px 24px;
     border-radius: 4px;
     box-shadow: 0 2px 8px rgba(0, 0, 0, 0.2);
     z-index: 9999;
+  }
+
+  .error {
+    background-color: #f44336;
+    color: white;
+  }
+
+  .success {
+    background-color: #9fdb5e;
+    color: black;
   }
   
   .slide-fade-enter-active,
