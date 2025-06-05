5a99651de3c495b80906cca1f5b23e66892e7278
james.barnes <james.barnes@solirius.com>
2025-06-05 11:49:09 +0100

    move login view into component

---
+++ src/components/LoginComponent.vue
@@ -0,0 +1,68 @@
+<script lang="ts">
+    import {ref} from 'vue';
+    import { useRouter } from 'vue-router';
+    import api from '../api';
+
+    export default {
+        name: 'LoginComponent',
+        props: {},
+        components: {},
+        setup(props, {emit}){
+            const usernameInput = ref('');
+            const passwordInput = ref('');
+
+            const router = useRouter();
+
+            function resetFields(){
+                usernameInput.value = '';
+                passwordInput.value = '';
+            }
+
+            async function login(){
+                const result = await api.login({username: usernameInput.value, password: passwordInput.value});
+                if(!result.success){
+                    resetFields();
+                    return emit('error', result.message);
+                }
+                //set small delay to ensure token is stored properly
+                setTimeout(() => {
+                    emit('success', result.message);
+                    router.push('/');
+                }, 300);
+            }
+            return {
+                usernameInput,
+                passwordInput,
+                login
+            }
+        }
+    }
+</script>
+
+<template>
+    <div class="form-container">
+        <div class="input-row">
+            <label>Username</label>
+            <v-tooltip location="top" text="Username must only consist of letters, digits and underscores and be at least 3 characters long">
+                <template v-slot:activator="{ props }">
+                    <input v-bind="props" v-model="usernameInput" type="text">
+                </template>
+            </v-tooltip>
+        </div>
+        <div class="input-row">
+            <label>Password</label>
+            <v-tooltip location="top" text="Password must contain at least one uppercase letter, lowercase letter and digit and be at least 6 characters long">
+                <template v-slot:activator="{ props }">
+                    <input v-bind="props" v-model="passwordInput" type="text">
+                </template>
+            </v-tooltip>
+        </div>
+    </div>
+    <span class="button submit-button" @click="login">Login</span>
+</template>
+
+<style scoped>
+    .form-container {
+        width: 40%;
+    }
+</style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,15 +1,10 @@
 <script setup lang="ts">
     import { ref, computed } from 'vue';
-    import { useRouter } from 'vue-router';
     import RegistrationComponent from '../components/RegistrationComponent.vue';
-    import api from '../api';
+    import LoginComponent from '../components/LoginComponent.vue';
 
     // quirk of script setup - need this to be able to use emits
     const emit = defineEmits();
-    const router = useRouter();
-
-    const usernameInput = ref('');
-    const passwordInput = ref('');
 
     const showRegister = ref(false);
     const registrationButtonName = computed(() => showRegister.value? 'Hide': 'Show registration');
@@ -17,52 +12,19 @@
         showRegister.value = !showRegister.value;
     }
 
-    function resetFields(){
-        usernameInput.value = '';
-        passwordInput.value = '';
-    }
-
     function successfulRegister(message: string){
         showRegister.value = false;
         emit('success', message);
-    }
-
-    async function login(){
-        const result = await api.login({username: usernameInput.value, password: passwordInput.value});
-        if(!result.success){
-            resetFields();
-            return emit('error', result.message);
-        }
-        //set small delay to ensure token is stored properly
-        setTimeout(() => {
-            emit('success', result.message);
-            router.push('/');
-        }, 300);
     }
 </script>
 
 <template>
     <div>
         <h1 class="page-title">Welcome to Feedback Manager</h1>
-        <div class="form-container">
-            <div class="input-row">
-                <label>Username</label>
-                <v-tooltip location="top" text="Username must only consist of letters, digits and underscores and be at least 3 characters long">
-                    <template v-slot:activator="{ props }">
-                        <input v-bind="props" v-model="usernameInput" type="text">
-                    </template>
-                </v-tooltip>
-            </div>
-            <div class="input-row">
-                <label>Password</label>
-                <v-tooltip location="top" text="Password must contain at least one uppercase letter, lowercase letter and digit and be at least 6 characters long">
-                    <template v-slot:activator="{ props }">
-                        <input v-bind="props" v-model="passwordInput" type="text">
-                    </template>
-                </v-tooltip>
-            </div>
-        </div>
-        <span class="button submit-button" @click="login">Login</span>
+        <LoginComponent
+         @error="(message) => {$emit('error', message)}"
+         @success="(message) => {$emit('success', message)}"
+        />
         <h3>No account? <span class="button" @click="toggleRegister">{{ registrationButtonName }}</span></h3>
         <RegistrationComponent 
         @error="(message) => {$emit('error', message)}" 
@@ -73,7 +35,5 @@
 </template>
 
 <style scoped>
-    .form-container {
-        width: 40%;
-    }
+
 </style>
