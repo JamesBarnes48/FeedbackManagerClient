b148533c55cf728e2a5d66628a5981fe850b1e14
james.barnes <james.barnes@solirius.com>
2025-06-05 16:26:55 +0100

    collect property values together to use for aggregation like average rating

--- src/components/OverviewComponent.vue
+++ src/components/OverviewComponent.vue
@@ -1,7 +1,7 @@
 <script lang="ts">
     import type { Feedback } from '../interfaces/Feedback';
     import type { PropType } from 'vue';
-    import { computed } from 'vue';
+    import { watch, computed, ref } from 'vue';
 
     export default {
         name: 'OverviewComponent',
@@ -10,11 +10,24 @@
             feedbackArray: {type: Array as PropType<Feedback[]>, default(){return []}}
         },
         setup(props) {
-            const getStuff = () => {
-                props.feedbackArray.forEach((arr) => {console.log(arr)});
-            }
+            //watch feedback and group up properties for each manipulation
+            const collectedValues = ref<Record<string, any>>({rating: [], expectation: [], details: [], addedBy: []});
+            watch(() => props.feedbackArray, (newFeedbackArray) => {
+                newFeedbackArray.forEach((newFeedback) => {
+                    newFeedback.getFields().forEach((field) => {
+                        if(!newFeedback[field as keyof Feedback]) return;
+                        collectedValues.value[field] = collectedValues.value[field]? [...collectedValues.value[field], newFeedback[field as keyof Feedback]]: [newFeedback[field as keyof Feedback]];
+                    })
+                });
+            });
+
+            const averageRating = computed(() => {
+                const sum = collectedValues.value['rating'].reduce((acc: number, current: number) => acc + +current, 0);
+                return (sum / collectedValues.value['rating'].length) || 'N/A';
+            })
             return {
-                getStuff
+                collectedValues,
+                averageRating
             }
         }
     }
@@ -22,7 +35,11 @@
 
 <template>
     <div class="overview-container">
-        <h3 @click="getStuff">do it</h3>
+        <h3 @click="() => {console.log(collectedValues)}">do it</h3>
+        <div class="input-row">
+            <h3>Average Rating:</h3>
+            <p>{{ averageRating }}</p>
+        </div>
     </div>
 </template>
 
